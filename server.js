2d5ac495128ec2b2e65eb6194a50fb86958b1297
mark <markfarl@gmail.com>
2018-12-07 09:57:28 +0000

    prod

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const routes = require('./routes')
 //DEV
 //const dev = process.env.NODE_ENV !== 'production'
 //Production
-const dev = 1 == 1
+const dev = 1 !== 1
 const app = next({ dev })
 const handler = routes.getRequestHandler(app)
 
 const { createServer } = require('http')
 app.prepare().then(() => {
-  createServer(handler).listen(3001),
+  createServer(handler).listen(3000),
     err => {
       if (err) throw err
-      console.log('Ready on localhost:80')
+      console.log('Ready on localhost:3000')
       console.log(process.env.NODE_ENV)
     }
 })
